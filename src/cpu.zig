4f58a8e373a448626bb2d283df74a7b6f86ea369
Petar Obradovic <perazmn@gmail.com>
2025-06-25 13:05:42 +0200

    Make flags register a struct

--- src/cpu.zig
+++ src/cpu.zig
@@ -1,5 +1,20 @@
 const std = @import("std");
-const Cpu = struct {
+
+const FlagsRegister = packed struct {
+    rest: u4 = 0,
+    carry: bool,
+    half_carry: bool,
+    sub: bool,
+    zero: bool,
+    pub fn getByte(self: FlagsRegister) u8 {
+        return @bitCast(self);
+    }
+    pub fn setByte(self: *FlagsRegister, value: u8) void {
+        self.* = @bitCast(value);
+    }
+};
+
+pub const Cpu = struct {
     A: u8,
     B: u8,
     C: u8,
@@ -7,16 +22,16 @@ const Cpu = struct {
     E: u8,
     H: u8,
     L: u8,
-    F: u8,
+    F: FlagsRegister,
     pc: u16,
     sp: u16,
 
     pub fn getAF(self: *const Cpu) u16 {
-        return @as(u16, @intCast(self.A)) << 8 | @as(u16, @intCast(self.F));
+        return @as(u16, @intCast(self.A)) << 8 | @as(u16, @intCast(self.F.getByte()));
     }
     pub fn setAF(self: *Cpu, value: u16) void {
         self.A = (value & 0xFF00) >> 8;
-        self.F = (value & 0xFF);
+        self.F.setByte(value & 0xFF);
     }
     pub fn getBC(self: *const Cpu) u16 {
         return @as(u16, @intCast(self.B)) << 8 | @as(u16, @intCast(self.C));
@@ -40,3 +55,29 @@ const Cpu = struct {
         self.L = (value & 0xFF);
     }
 };
+
+test "Flags register as byte" {
+    const f = FlagsRegister{
+        .zero = true,
+        .sub = false,
+        .half_carry = true,
+        .carry = false,
+    };
+
+    try std.testing.expectEqual(0b10100000, f.getByte());
+}
+
+test "Flags register set byte" {
+    var f = FlagsRegister{
+        .zero = true,
+        .sub = false,
+        .half_carry = true,
+        .carry = false,
+    };
+    f.setByte(0b01100000);
+
+    try std.testing.expectEqual(false, f.zero);
+    try std.testing.expectEqual(true, f.sub);
+    try std.testing.expectEqual(true, f.half_carry);
+    try std.testing.expectEqual(false, f.carry);
+}
