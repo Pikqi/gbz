4005efc2a798588d16638f6d9187219a7d734005
Petar Obradovic <perazmn@gmail.com>
2026-06-26 14:34:02 +0200

    Add shell.nix

---
+++ shell.nix
@@ -0,0 +1,40 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    zig_0_15
+    pkg-config
+
+    # OpenGL / GLX
+    libGL
+    mesa
+
+    # Vulkan
+    vulkan-loader
+    vulkan-headers
+    shaderc
+
+    # X11
+    xorg.libX11
+    xorg.libXrandr
+    xorg.libXinerama
+    xorg.libXcursor
+    xorg.libXi
+    xorg.libXext
+
+    # Wayland
+    wayland
+    libxkbcommon
+
+    # Audio
+    alsa-lib
+  ];
+
+  LD_LIBRARY_PATH = with pkgs; lib.makeLibraryPath [
+    libGL
+    mesa
+    vulkan-loader
+    xorg.libX11
+    wayland
+  ];
+}
